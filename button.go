94a55b72efb31babbc663c52548ae76d7ce54ec1
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2020-10-16 06:52:24 +0900

    Button: handle KeyLeft and KeyRight

--- button.go
+++ button.go
@@ -4,6 +4,7 @@
 package tviewx
 
 import (
+    "github.com/gdamore/tcell"
     "github.com/rivo/tview"
 )
 
@@ -13,4 +14,17 @@ type Button struct {
 
 func NewButton(label string) *Button {
     return &Button{ Button: tview.NewButton(label) }
+}
+
+func (b *Button)InputHandler() func(*tcell.EventKey, func(tview.Primitive)) {
+    return b.WrapInputHandler(func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+	e := event
+	switch key := event.Key(); key {
+	case tcell.KeyLeft:
+	    e = tcell.NewEventKey(tcell.KeyBacktab, event.Rune(), event.Modifiers())
+	case tcell.KeyRight:
+	    e = tcell.NewEventKey(tcell.KeyTab, event.Rune(), event.Modifiers())
+	}
+	b.Button.InputHandler()(e, setFocus)
+    })
 }
