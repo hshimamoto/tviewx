8bb36d485929381bed5543dfbacf61212c6911b8
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2021-09-15 07:27:01 +0900

    Flex: correct unfocus condition

--- flex.go
+++ flex.go
@@ -113,7 +113,7 @@ func (f *Flex)Focus(delegate func(p tview.Primitive)) {
 	    cur := f.focused
 	    n := (cur + i + sz) % sz
 	    for n != cur {
-		if n == 0 {
+		if (n == 0 && i == 1) || (n == (sz - 1) && i == -1) {
 		    if f.blurFunc != nil {
 			f.focus = nil
 			f.blurFunc(key)
